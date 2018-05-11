45a1385bfe29e62433ca6b07cbe14c4e2434f930
sedhakobyan <sedrakhakobyan@gmail.com>
2018-05-11 16:28:20 +0400

    work with progressbar version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -32,83 +32,85 @@ public class Controller {
     private ProgressBar progress=new ProgressBar();
     @FXML
 
-        private void ClickBtn(ActionEvent event) {
-        event.consume();
-        progress.progressProperty().bind(task.progressProperty());
-        task.run();
+        private void ClickBtn() {
+
+        progress.setVisible(true);
+        Task<Void> task = new Task<Void>(){
+            @Override
+
+            public Void call() throws Exception {
+
+                    System.out.println("xcvbn");
+                    DesiredCapabilities caps = new DesiredCapabilities();
+                    caps.setBrowserName("TTUJur");
+                    caps.setJavascriptEnabled(true);
+                    caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                            "C:\\Users\\SedHakobyan\\Desktop\\phantomjs.exe");
+
+                    WebDriver mydrv = new PhantomJSDriver(caps);
+
+                    mydrv.get("https://www.myscore.com.ua/");
+                    mydrv.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+                    //WebElement elo = mydrv.findElement(By.cssSelector("td.cell_sa.score")); //#g_1_zmlHDRz3 > td.cell_sa.score
+                    WebElement elo = mydrv.findElements(By.cssSelector("td.cell_sa.score")).get(1);
+                    mydrv.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+                    System.out.println("tag name =" + "size = " + mydrv.findElements(By.cssSelector("td.cell_sa.score")).size());
+                    elo.click();
 
 
-
-    }
-    private Task task = new Task() {
-        @Override
-        protected Object call() throws Exception {
-
-            Trade.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-                System.out.println("xcvbn");
-                DesiredCapabilities caps = new DesiredCapabilities();
-                caps.setBrowserName("TTUJur");
-                caps.setJavascriptEnabled(true);
-                caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-                        "C:\\Users\\SedHakobyan\\Desktop\\phantomjs.exe");
-
-                WebDriver mydrv = new PhantomJSDriver(caps);
-
-                mydrv.get("https://www.myscore.com.ua/");
-                mydrv.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
-                //WebElement elo = mydrv.findElement(By.cssSelector("td.cell_sa.score")); //#g_1_zmlHDRz3 > td.cell_sa.score
-                WebElement elo = mydrv.findElements(By.cssSelector("td.cell_sa.score")).get(1);
-                mydrv.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
-                System.out.println("tag name =" + "size = " + mydrv.findElements(By.cssSelector("td.cell_sa.score")).size());
-                elo.click();
-
-
-                try {
-                    WebDriverWait wait = new WebDriverWait(mydrv, 30);
-
-                    Actions ob = new Actions(mydrv);
-                    ob.click(elo);
-                    Action action = ob.build();
-                    action.perform();
-                    mydrv.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
-                    int i = 0;
-                    System.out.println("home name = " + elo.getTagName());
-                    List<String> hw = new ArrayList<String>();
-                    for (String st : mydrv.getWindowHandles()) {
-                        hw.add(st);
-                        i++;
-                        mydrv.switchTo().window(st);
-                        System.out.println("link =" + mydrv.getCurrentUrl());
-                        FileUtils.copyFile(((TakesScreenshot) mydrv).getScreenshotAs(OutputType.FILE), new File("d:\\myscore.com.ua.jpeg" + i));
-                        System.out.println(mydrv.getWindowHandles().size());
-
-                    }
-
-                    mydrv.switchTo().window(hw.get(1));
-                    WebElement elo1 = mydrv.findElement(By.cssSelector("#a-match-odds-comparison"));
-                    elo1.click();
                     try {
-                        Thread.sleep((1000));
-                    } catch (InterruptedException e) {
+                        WebDriverWait wait = new WebDriverWait(mydrv, 30);
 
+                        Actions ob = new Actions(mydrv);
+                        ob.click(elo);
+                        Action action = ob.build();
+                        action.perform();
+                        mydrv.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+                        int i = 0;
+                        System.out.println("home name = " + elo.getTagName());
+                        List<String> hw = new ArrayList<String>();
+                        for (String st : mydrv.getWindowHandles()) {
+                            hw.add(st);
+                            i++;
+                            mydrv.switchTo().window(st);
+                            System.out.println("link =" + mydrv.getCurrentUrl());
+                            FileUtils.copyFile(((TakesScreenshot) mydrv).getScreenshotAs(OutputType.FILE), new File("d:\\myscore.com.ua.jpeg" + i));
+                            System.out.println(mydrv.getWindowHandles().size());
+
+                        }
+
+                        mydrv.switchTo().window(hw.get(1));
+                        WebElement elo1 = mydrv.findElement(By.cssSelector("#a-match-odds-comparison"));
+                        elo1.click();
+                        try {
+                            Thread.sleep((1000));
+                        } catch (InterruptedException e) {
+
+                        }
+                        FileUtils.copyFile(((TakesScreenshot) mydrv).getScreenshotAs(OutputType.FILE), new File("d:\\myscore.com.ua.jpeg" + 6));
+
+                        WebElement part = mydrv.findElement(By.cssSelector("#odds_1x2 > tbody > tr"));
+                        System.out.println("root element text = " + part.findElement(By.cssSelector("td.bookmaker > div > a")).getAttribute("title").toString()); //System.out.println("root element text = "+ mydrv.findElement(By.cssSelector("#odds_1x2 > tbody > tr:nth-child(2) > td.bookmaker > div > a")).getAttribute("title").toString()+ "|||| "+ mydrv.findElement(By.cssSelector("#odds_1x2 > tbody > tr:nth-child(2) > td.kx > span")).getAttribute("class").toString() );
+
+
+                    } catch (IOException e)
+
+                    {
+                        e.printStackTrace();
                     }
-                    FileUtils.copyFile(((TakesScreenshot) mydrv).getScreenshotAs(OutputType.FILE), new File("d:\\myscore.com.ua.jpeg" + 6));
 
-                    WebElement part = mydrv.findElement(By.cssSelector("#odds_1x2 > tbody > tr"));
-                    System.out.println("root element text = " + part.findElement(By.cssSelector("td.bookmaker > div > a")).getAttribute("title").toString()); //System.out.println("root element text = "+ mydrv.findElement(By.cssSelector("#odds_1x2 > tbody > tr:nth-child(2) > td.bookmaker > div > a")).getAttribute("title").toString()+ "|||| "+ mydrv.findElement(By.cssSelector("#odds_1x2 > tbody > tr:nth-child(2) > td.kx > span")).getAttribute("class").toString() );
-                 progress.progressProperty().unbind();
-
-                } catch (IOException e)
-
-                {
-                    e.printStackTrace();
-                }
-            });
-
-            return true;
-        }
-    };
-
+                progress.setVisible(false);
+                return null;
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
+        progress.progressProperty().unbind();
 
     }
+
+
+}
+
+
 
